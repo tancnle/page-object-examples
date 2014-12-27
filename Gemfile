42a16c9777c0812a360b9c76655d6982f12dfe35
Tan Le <tanle.oz@gmail.com>
2014-12-27 17:18:16 +1100

    Added support for collection matchers

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rake'
 
 gem 'rspec'
+gem 'rspec-collection_matchers'
 gem 'cucumber'
 gem 'selenium-webdriver'
 gem 'page-object'
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,5 @@
 require 'rspec/expectations'
+require 'rspec/collection_matchers'
 require 'selenium-webdriver'
 require 'page-object'
 require 'page-object/page_factory'
