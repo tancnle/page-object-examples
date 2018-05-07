8fc76a9e37af65defe7a724ea8e0530c6537c2cd
Tan Le <tanle.oz@gmail.com>
2018-05-07 23:27:45 +1000

    Refactor PersistentBrowser.
    - Replace timeout with read_timeout and open_timeout
    - Switch to chrome driver
    - Remove un-needed `quit` method

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,7 +1,3 @@
 Before do
-  @browser = PageObject::PersistantBrowser.get_browser
-end
-
-at_exit do
-  PageObject::PersistantBrowser.quit
+  @browser = PageObject::PersistantBrowser.browser
 end
--- features/support/persistent_browser.rb
+++ features/support/persistent_browser.rb
@@ -1,19 +1,12 @@
 module PageObject
   module PersistantBrowser
-
-    @@browser = false
-
-    def self.get_browser
-      if !@@browser
+    def self.browser
+      @_browser ||= begin
         client = Selenium::WebDriver::Remote::Http::Default.new
-        client.timeout = 100
-        @@browser = Selenium::WebDriver.for :firefox, :http_client => client
+        client.open_timeout = 100
+        client.read_timeout = 100
+        Selenium::WebDriver.for :chrome, http_client: client
       end
-      @@browser
-    end
-
-    def self.quit
-      @@browser.close
     end
   end
 end
