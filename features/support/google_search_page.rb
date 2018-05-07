bdc1a9cb2ce71533fdfcecd880de3f8827b0524a
Tan Le <tanle.oz@gmail.com>
2018-05-07 23:29:34 +1000

    Update CSS lookup for Google Search button.

--- features/support/google_search_page.rb
+++ features/support/google_search_page.rb
@@ -4,17 +4,17 @@ class GoogleSearchPage
   page_url 'http://www.google.com.au'
 
   text_field(:search_field, name: 'q')
-  button(:search, name: 'btnG')
+  button(:search, name: 'btnK')
 
   def search_for(search_query)
     self.search_field = search_query
     search
     wait_until(60, 'Google search is so slooooooow!') do
-      self.text[/About .* results \(.* seconds\)/]
+      text[/About .* results \(.* seconds\)/]
     end
   end
 
   def search_results
-    self.link_elements(css: '.r>a').map(&:text)
+    link_elements(css: '.r>a').map(&:text)
   end
 end
