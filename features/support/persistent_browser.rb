3ae8013e4c9a97957029279e5f8cb93d6ed33392
Tan Le <tanle.oz@gmail.com>
2018-05-10 08:51:08 +1000

    Run test in Chrome headless.

--- features/support/persistent_browser.rb
+++ features/support/persistent_browser.rb
@@ -5,8 +5,15 @@ module PageObject
         client = Selenium::WebDriver::Remote::Http::Default.new
         client.open_timeout = 100
         client.read_timeout = 100
-        Selenium::WebDriver.for :chrome, http_client: client
+
+        Selenium::WebDriver.for :chrome, http_client: client, options: options
       end
+    end
+
+    def self.options
+      args = %w[headless disable-gpu]
+
+      Selenium::WebDriver::Chrome::Options.new(args: args)
     end
   end
 end
